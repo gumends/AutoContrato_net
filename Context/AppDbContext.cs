02fd1a7ad6ba6841211e81f6508e3c3ceb0fe2e3
gumends <gustavo08.mendes@gmail.com>
2025-03-28 02:58:15 -0300

    Ajeitando o context para receber novas entidades

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,34 @@
+using AutoContrato_net.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoContrato_net.Context;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Proprietario> Proprietarios { get; set; }
+
+    public DbSet<Propriedade> Propriedades { get; set; }
+
+    public DbSet<Locatario> Locatarios { get; set; }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Locatario>()
+            .HasOne(l => l.Propriedades)
+            .WithOne(p => p.Locatario)
+            .HasForeignKey<Locatario>(l => l.PropriedadeId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Propriedade>()
+            .HasOne(p => p.Proprietario)
+            .WithMany(pr => pr.Propriedades)
+            .HasForeignKey(p => p.ProprietarioId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+
+}
