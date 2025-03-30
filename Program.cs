ba15f4e385bad28dad82bea4453786167a5deb17
gumends <gustavo08.mendes@gmail.com>
2025-03-30 01:00:12 -0300

    Configurando redundâncias de respostas

--- Program.cs
+++ Program.cs
@@ -9,12 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
-// Add services to the container.
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     });
+
+
 
 builder.Services.AddScoped<UsuariosServices>();
 builder.Services.AddScoped<ProprietarioService>();
